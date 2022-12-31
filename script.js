e70c50b2709d571458e9a69e0b8a872412f31261
leticea <nascimento.leticeah@gmail.com>
2022-12-31 10:07:10 -0300

    criando const no js

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ const updateData = (element, message) => {
   element.textContent = message;
 };
 
+const init = () => {
+  gameHealth = 100;
+  gameOver = false;
+  randomGuessNumber = Math.trunc(Math.random() * 10) + 1;
+  updateData(gameHealthNumberElement, "100%");
+  updateData(gameFeedbackElement, "What is your guess?");
+  updateData(gameNumberElement, "?");
+  gameGuessElement.value = "";
+  gameHealthElement.style.background = "green";
+  gameHealthElement.style.width =
+
+
+};
+
