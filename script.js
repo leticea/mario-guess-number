0986a0f250fd23718e9e562072c0fafb4c3557ca
leticea <nascimento.leticeah@gmail.com>
2022-12-31 09:50:10 -0300

    criando const no js

---
+++ script.js
@@ -0,0 +1,7 @@
+const gameNumberElement = document.querySelector(".game-number");
+const gameFeedbackElement = document.querySelector(".game-feedback");
+const gameGuessElement = document.querySelector(".game-guess");
+const gameHealthNumberElement = document.querySelector(".game-health-number");
+const gameHealthElement = document.querySelector(".game-health-bar");
+const gamePlayBtn = document.querySelector(".game-button-play");
+const gameResetBtn = document.querySelector(".game-button-reset");
